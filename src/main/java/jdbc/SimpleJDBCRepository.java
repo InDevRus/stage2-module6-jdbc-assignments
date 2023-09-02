343eee13f64dff20c5f3701caecfb27101c7ec00
indev <InDevRus@hotmail.com>
2023-09-02 05:23:07 +0000

    Fixed SimpleJDBCRepository createUser method

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -21,7 +21,7 @@ public class SimpleJDBCRepository {
     private Statement statement = null;
 
     private final DataSource dataSource = CustomDataSource.getInstance();
-    private static final String CREATE_USER_SQL = "insert into myusers (id, firstname, lastname, age) values (?, ?, ?, ?);";
+    private static final String CREATE_USER_SQL = "insert into myusers (firstname, lastname, age) values (?, ?, ?);";
     private static final String UPDATE_USER_SQL = "update myusers user set firstname = ?, lastname = ?, age = ? where id = ?";
     private static final String DELETE_USER = "delete from myusers where id = ?";
     private static final String FIND_USER_BY_ID_SQL = "select id, firstname, lastname, age from myusers where id = ?";
@@ -33,9 +33,9 @@ public class SimpleJDBCRepository {
     public Long createUser(User user) {
         try (var connection = dataSource.getConnection()) {
             var creationStatement = connection.prepareStatement(CREATE_USER_SQL, Statement.RETURN_GENERATED_KEYS);
-            creationStatement.setString(2, user.getFirstName());
-            creationStatement.setString(3, user.getLastName());
-            creationStatement.setInt(4, user.getAge());
+            creationStatement.setString(1, user.getFirstName());
+            creationStatement.setString(2, user.getLastName());
+            creationStatement.setInt(3, user.getAge());
             creationStatement.executeUpdate();
 
             var keys = creationStatement.getGeneratedKeys();
